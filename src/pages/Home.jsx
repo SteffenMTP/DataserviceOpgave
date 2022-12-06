3de5b51a0bd789be21c196d36e5516f03a31b94c
Steffen Monphichai Tersbøl Petersen <stef303n@videndjurs.net>
2022-12-06 13:47:37 +0100

    version2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import Title from "../components/Title";
-
+// import {gsap} from 'gsap'
 
 const Home = () => {
+
+    // const glass = document.getElementById('glass');
+
+    //     const tl = gsap.timeline({defaults: {ease: "power2.inOut", duration: 1.5}})
+
+    //     tl.from('img', {x: '-10%', opacity: 0})
+    //     .from('.container', {opacity: 0, delay: .5, duration: 1}, "-=1.5")
+    //     .from('.container', {x:'-20%', backdropFilter: 'blur(0px)'})
+    //     .from('.seq', {y: -30, opacity: 0, stagger: .2, duration: .5}, "-=.5")
+    //     .from('h1', {y: 20, clipPath: 'inset(0 0 100% 0'}, "-=.8")
+
 
     return (
         <>
@@ -9,16 +20,15 @@ const Home = () => {
             <section className="container">
                 <Title headline="Home" />
 
-                <div className="row">
-                    <div className="col">
-                        <p className="">
-                            Velkommen til dette projekt
-                        </p>
+                {/* <img src="./assets/img/abstract.jpg" alt="abstract background corridor" class="glassImg"/>
+                    <div class="circle"></div>
 
+                    <div class="Glasscontainer" id="glass">
+                        <h2 class="seq">Hello everyone</h2>
+                        <p class="seq">Let's go ahead and get this show on the road</p>
                     </div>
 
-
-                </div>
+                    <h1>Glassmorphism Realized</h1> */}
 
             </section>
 
